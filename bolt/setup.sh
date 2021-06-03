680c4cf5dc5b9303670724c79e65e91cc8e8c885
Chen <chuang5@apple.com>
2021-06-03 12:28:51 -0700

    U: assign pytorch version

--- bolt/setup.sh
+++ bolt/setup.sh
@@ -8,7 +8,7 @@ echo_time() {
 
 echo_time "Installing Unix dependencies."
 apt-get update
-apt-get install -y libsm6 libxext6
+apt-get install -y ffmpeg libsm6 libxext6
 apt-get install -y libxrender-dev
 apt-get install -y tmux
 
@@ -24,7 +24,6 @@ pip3 config set global.index-url 'https://pypi.apple.com/simple'
 pip3 config set global.extra-index-url 'https://pypi.python.org/simple'
 
 echo_time "Install additional libraries for NSVF"
-pip3 install --upgrade torchvision
 pip3 install --upgrade awscli
 pip3 install --upgrade tensorboard
 pip3 install --upgrade turibolt
@@ -34,6 +33,7 @@ then
   echo_time "Install install NSVF"
   git clone https://github.com/rocksat/nsvf.pytorch.git --recursive
   cd nsvf.pytorch
+  pip3 install torch==1.4.0 torchvision==0.5.0
   pip3 install -r requirements.txt
   python3 setup.py build_ext --inplace
 fi
